6797d7adfc97f8ea54533e65e502ed6a7ee67cfb
mohan <sahumohan338@gmail.com>
2022-09-13 13:52:23 +0530

    [mohan]add: Read number and Display Week

---
+++ JsPracticeProblem.js
@@ -0,0 +1,37 @@
+var Val1=1;
+var Val2=2;
+var Val3=3;
+var Val4=4;
+var Val5=5;
+var Val6=6;
+var Val7=7;
+if(Val1==1)
+{
+  console.log("Monday");
+}else if(Val2==2)
+{
+  console.log("Tuesday");
+}
+else if(Val3==3)
+{
+  console.log("Wednesday");
+}
+else if(Val4==4)
+{
+  console.log("Thrusday");
+}
+else if(Val5==5)
+{
+  console.log("Friday");
+}
+else if(Val6==6)
+{
+  console.log("Suturday");
+}
+else if(Val6==6)
+{
+  console.log("Sunday");
+}
+else{
+  console.log("not found");
+}
