cee1826e94ed4d1596323e1f5a8ff0242b914131
mohan <sahumohan338@gmail.com>
2022-09-13 15:44:28 +0530

    [mohan]add: Read number and Display Week Day

--- JsPracticeProblem.js
+++ JsPracticeProblem.js
@@ -2,19 +2,31 @@ var Val1=1;
 var Val2=2;
 var Val3=3;
 var Val4=4;
-let x=1;
+var Val5=5;
+var Val6=6;
+var Val7=7;
+let x=2;
 switch(x)
 {
     case Val1:
-        console.log("One");
+        console.log("Sunday");
         break;
         case Val2:
-            console.log("Two");
+            console.log("Monday");
         break;
     case Val3:
-            console.log("Three");
+            console.log("Tuesday");
         break;
     case Val4:
-            console.log("Four");
+            console.log("Wednesday");
+         break;
+         case Val5:
+            console.log("thrusday");
+         break;
+         case Val6:
+            console.log("Friday");
+         break;
+         case Val7:
+            console.log("Saturday");
          break;
 }
